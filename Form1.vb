6127dfd81c1f2c0f92df2930c6421a99209ce90a
Alvin Cheng <eventide1029@gmail.com>
2025-03-13 10:49:44 +1100

    Added guard clauses and fixed is numeric

--- Form1.vb
+++ Form1.vb
@@ -3,38 +3,47 @@
     ' Invalid conditions:
     ' 1. If either of the textboxes are empty
     ' 2. If either of the textboxes contain a non-numeric value
-    Private Sub Check()
+    Private Function Check()
         If addend1.Text = "" Or addend2.Text = "" Then
             MsgBox("Please enter a number in both boxes")
+            Return False
         End If
-        If (Not (IsNumeric(addend1))) Or (Not (IsNumeric(addend2))) Then
+        If (Not (IsNumeric(addend1.Text))) Or (Not (IsNumeric(addend2.Text))) Then
             MsgBox("Please enter a VALID number in both boxes")
+            Return False
         End If
 
-    End Sub
+        Return True
+    End Function
 
     ' Addition
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Check()
-        ans.Text = Val(addend1.Text) + Val(addend2.Text)
+        If Check() Then
+            ans.Text = Val(addend1.Text) + Val(addend2.Text)
+        End If
     End Sub
 
     ' Subtraction
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Check()
-        ans.Text = Val(addend1.Text) - Val(addend2.Text)
+        If Check() Then
+            ans.Text = Val(addend1.Text) - Val(addend2.Text)
+        End If
     End Sub
 
     ' Multiplication
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        Check()
-        ans.Text = Val(addend1.Text) * Val(addend2.Text)
+        If Check() Then
+            ans.Text = Val(addend1.Text) * Val(addend2.Text)
+        End If
     End Sub
 
     ' Division
-    ' Special case: Cannot divide by zero
+    ' Special edge case: Cannot divide by zero (Will report and stop process)
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Check()
+        If Not Check() Then
+            Exit Sub
+        End If
+
         If addend2.Text = "0" Then
             MsgBox("Cannot divide by zero")
             Exit Sub
