5dcba986a7ab6767b460e4a7023b31a564c31775
Alvin Cheng <eventide1029@gmail.com>
2025-03-12 14:46:09 +1100

    Finish task

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,108 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        addend1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        addend2 = New TextBox()
+        Label1 = New Label()
+        ans = New TextBox()
+        SuspendLayout()
+        ' 
+        ' addend1
+        ' 
+        addend1.Location = New Point(81, 289)
+        addend1.Name = "addend1"
+        addend1.Size = New Size(125, 38)
+        addend1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(285, 446)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(122, 90)
+        Button1.TabIndex = 1
+        Button1.Text = "/"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(285, 83)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(122, 90)
+        Button2.TabIndex = 2
+        Button2.Text = "+"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(285, 206)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(122, 90)
+        Button3.TabIndex = 3
+        Button3.Text = "-"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(285, 324)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(122, 90)
+        Button4.TabIndex = 4
+        Button4.Text = "*"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' addend2
+        ' 
+        addend2.Location = New Point(484, 289)
+        addend2.Name = "addend2"
+        addend2.Size = New Size(125, 38)
+        addend2.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(682, 296)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(32, 31)
+        Label1.TabIndex = 6
+        Label1.Text = "="
+        ' 
+        ' ans
+        ' 
+        ans.Location = New Point(791, 289)
+        ans.Name = "ans"
+        ans.Size = New Size(125, 38)
+        ans.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(14F, 31F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(995, 653)
+        Controls.Add(ans)
+        Controls.Add(Label1)
+        Controls.Add(addend2)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(addend1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents addend1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents addend2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ans As TextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form1
+    Sub Check()
+        If addend1.Text = "" Or addend2.Text = "" Then
+            MsgBox("Please enter a number in both boxes")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Check()
+        ans.Text = Val(addend1.Text) + Val(addend2.Text)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Check()
+        ans.Text = Val(addend1.Text) - Val(addend2.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Check()
+        ans.Text = Val(addend1.Text) * Val(addend2.Text)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Check()
+        ans.Text = Val(addend1.Text) / Val(addend2.Text)
+    End Sub
+
 
 End Class
